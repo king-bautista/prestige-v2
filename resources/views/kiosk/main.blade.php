9b4139b95f0983bfa40a64b1c34c5a353ff92efa
Ray <ray.onsitedigital@gmail.com>
2023-03-06 09:08:22 +0800

    WIP

--- resources/views/kiosk/main.blade.php
+++ resources/views/kiosk/main.blade.php
@@ -13,7 +13,7 @@
                     <router-view></router-view>
                 </div>
             </div>            
-            <div class="row">
+            <!-- <div class="row">
                 <div class="col-md-12 text-center pt-2 pr-136">
                     <router-link to="/">
                         <div class="h-button widget-button home-button active logs" data-link='Home'>
@@ -41,7 +41,7 @@
                         </div>
                     </router-link>
                 </div>
-            </div>
+            </div> -->
             <div id="popover-content" class="hide d-none" style="z-index:1">
                 Need help? Touch here.
             </div>
@@ -129,7 +129,7 @@
 
     });
 
-    $('.home-button').click();
+    // $('.home-button').click();
     
 </script>
 @endpush
