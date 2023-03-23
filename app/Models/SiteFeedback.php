f63ea04e15946662c75125fdbf2dff8fcc4867c1
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-03-23 17:21:13 +0800

    updates

---
+++ app/Models/SiteFeedback.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SiteFeedback extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'site_id',
+        'site_screen_id',
+        'helpful',
+        'reason',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'deleted_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+    */
+    protected $table = 'site_feedback';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+}
