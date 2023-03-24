28a2a6fde8dd86ceac654411122bfe33a9c87de5
jordanperenaprestige <jperena.prestigeinteractive@gmail.com>
2023-03-24 12:00:50 +0800

    added admin FAQ's

---
+++ resources/views/admin/faqs.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.admin.master')
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">FAQ's</h1>
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+          <li class="breadcrumb-item active">FAQ's</li>
+        </ol>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<admin-faqs></admin-faqs>
+<!-- /.content -->
+@stop
+
+@push('scripts')    
+@endpush
