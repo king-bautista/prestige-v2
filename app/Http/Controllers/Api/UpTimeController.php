64cc68cd8f9c48ac3801314d8af928bd6dc9e2c3
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-03-27 14:47:52 +0800

    updates

--- app/Http/Controllers/Api/UpTimeController.php
+++ app/Http/Controllers/Api/UpTimeController.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 
 use App\Models\SiteScreenUptime;
 use App\Models\SiteScreenUptimeTemp;
+use App\Models\ViewModels\SiteViewModel;
 
 class UpTimeController extends AppBaseController implements UpTimeControllerInterface
 {
@@ -16,11 +17,8 @@ class UpTimeController extends AppBaseController implements UpTimeControllerInte
     {
         try
         {
-            // checking last day logs
-            // summarize last day logs
-            // deleting last day logs
+            $this->yesterdayLogs($request->site_screen_id);
 
-            // inset new day logs            
             $data = [
                 'site_screen_id' => $request->site_screen_id,
                 'up_time_date' => date('Y-m-d'),
@@ -38,8 +36,35 @@ class UpTimeController extends AppBaseController implements UpTimeControllerInte
         }
     }
 
-    public function summirize(Type $var = null)
+    public function yesterdayLogs($site_screen_id)
     {
-        # code...
+        $yesterday = Carbon::yesterday()->format('Y-m-d');
+        $yesterday_first = SiteScreenUptimeTemp::where('up_time_date', $yesterday)->where('site_screen_id', $site_screen_id)->first();
+        if($yesterday_first) {
+            $yesterday_last = SiteScreenUptimeTemp::where('up_time_date', $yesterday)->where('site_screen_id', $site_screen_id)->latest()->first();
+            $uptime_start = Carbon::parse($yesterday_last->up_time_date.' '.$yesterday_last->up_time_hours);
+            $uptime_end = Carbon::parse($yesterday_first->up_time_date.' '.$yesterday_first->up_time_hours);    
+            $total_hours_up = $uptime_start->diffInHours($uptime_end);
+
+            $site = SiteViewModel::where('is_default', 1)->where('active', 1)->first();
+            $mall_hours_start = Carbon::parse($yesterday.' '.$site->details['time_from']);
+            $mall_hours_end = Carbon::parse($yesterday.' '.$site->details['time_to']);
+            $total_mall_hours = $mall_hours_start->diffInHours($mall_hours_end);
+
+            $data = [
+                'site_screen_id' => $site_screen_id,
+                'up_time_date' => $yesterday,
+                'total_hours' => $total_hours_up,
+                'opening_hour' => $mall_hours_start,
+                'closing_hour' => $mall_hours_end,
+                'hours_up' => $total_mall_hours,
+                'percentage_uptime' => round(($total_hours_up/$total_mall_hours)*100, 2),
+            ];
+
+            $screen_uptime = SiteScreenUptime::create($data);
+
+            if($screen_uptime)
+                SiteScreenUptimeTemp::where('up_time_date', $yesterday)->where('site_screen_id', $site_screen_id)->delete();
+        }
     }
 }
