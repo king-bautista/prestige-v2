07f689cc7d5850f123737eb1bea1ce0beef532ac
jordanperenaprestige <jperena.prestigeinteractive@gmail.com>
2023-03-28 13:47:49 +0800

    updated customer care

--- resources/views/portal/customer_care.blade.php
+++ resources/views/portal/customer_care.blade.php
@@ -3,19 +3,16 @@
 <h4>Create Ad</h4>
 <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
   <ol class="breadcrumb">
-  <li class="breadcrumb-item"><a href="javascript: void(0);">Prestige&nbsp;</a></li>
-  <li class="breadcrumb-item"><a href="javascript: void(0);">Interactive</a></li>
-  <li class="breadcrumb-item active">Create Add</li>
+    <li class="breadcrumb-item"><a href="javascript: void(0);">Prestige&nbsp;</a></li>
+    <li class="breadcrumb-item"><a href="javascript: void(0);">Interactive</a></li>
+    <li class="breadcrumb-item active">Create Add</li>
   </ol>
 </nav>
 @endsection
 @section('content')
 <!-- Main content -->
 <div class="row">
-  <div class="col-md-2">
-    @include('layout.portal.company-profile')
-  </div>
-  <div class="col-md-10">
+  <div class="col-md-12">
     <portal-customer-care></portal-customer-care>
   </div>
 </div>
@@ -23,5 +20,5 @@
 <!-- /.content -->
 @stop
 
-@push('scripts')    
+@push('scripts')
 @endpush
--- routes/portal.php
+++ routes/portal.php
@@ -280,9 +280,10 @@ Route::group(['middleware' => 'isClient:portal'], function () {
     Route::get('/portal/customer-cares', 'Portal\CustomerCareController@index')->name('portal.customer-care');
     Route::get('/portal/customer-care/list', 'Portal\CustomerCareController@list')->name('portal.customer-care.list');
     Route::post('/portal/customer-care/store', 'Portal\CustomerCareController@store')->name('portal.customer-care.store');
-    Route::get('/portal/customer-care/{id}', 'Portal\CustomerCareController@details')->where('id', '[0-9]+')->name('portal.customer-care.details');
+    Route::get('/portal/customer-care/details', 'Portal\CustomerCareController@details')->where('id', '[0-9]+')->name('portal.customer-care.details');
     Route::post('/portal/customer-care/update', 'Portal\CustomerCareController@update')->name('portal.customer-care.update');
     Route::get('/portal/customer-care/delete/{id}', 'Portal\CustomerCareController@delete')->where('id', '[0-9]+')->name('portal.customer-care.delete');
+    Route::get('/portal/customer-care/get-company', 'Portal\CustomerCareController@getCompany')->name('portal.customer-care.get-company');
 
     Route::get('/portal/faqs', 'Portal\FAQsController@index')->name('portal.faqs');
     Route::get('/portal/faqs/get-all', 'Portal\FAQsController@getAll')->name('portal.faq.get-all');
