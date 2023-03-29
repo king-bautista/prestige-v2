533e64707d312244ceaf93fb0d74148d47a02f40
jordanperenaprestige <jperena.prestigeinteractive@gmail.com>
2023-03-29 09:45:16 +0800

    Added Leaflet - a JavaScript library for interactive maps

--- resources/views/layout/portal/footer.blade.php
+++ resources/views/layout/portal/footer.blade.php
@@ -1,4 +1,20 @@
 <footer class="footer">
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2022 <a href="http://www.prestigeinteractive.com.ph/">PRESTIGE INTERACTIVE</a>.</strong> All rights reserved.
+    <div class="row">
+        @foreach($user->permissions as $permission)
+        <div class="col-sm-1">
+            <a href="{{ $permission->link }}"></i>{{ $permission->name }}</a>
+            @if(count($permission->sub_permissions) > 0)
+            <div>
+                @foreach($permission->sub_permissions as $sub_menu)
+                @if($sub_menu->can_view)
+                <a href="{{ $sub_menu->link }}">{{ $sub_menu->name }}</a>
+                @endif
+                @endforeach
+            </div>
+            @endif
+        </div>
+        @endforeach
+
+    </div>
+    <div> <strong>Copyright &copy; 2023 <a href="http://www.prestigeinteractive.com.ph/">PRESTIGE INTERACTIVE</a>.</strong> All rights reserved.</div>
 </footer>
