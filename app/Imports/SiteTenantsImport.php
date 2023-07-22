09987a8502feb20f22727ac544299495e13c0fe7
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-07-22 17:11:23 +0800

    updates

--- app/Imports/SiteTenantsImport.php
+++ app/Imports/SiteTenantsImport.php
@@ -28,6 +28,7 @@ class SiteTenantsImport implements ToCollection, WithHeadingRow
         foreach ($rows as $row) {
             if($row['store_name']) {
                 $brand_id = ($row['store_name']) ? $this->getBrandId($row['store_name']) : 0;
+                dd($brand_id);
                 $site_id = ($row['mall_name']) ? $this->getSiteId($row['mall_name']) : 0;
                 $building_id = ($row['building_name']) ? $this->getBuildingId($site_id, $row['building_name']) : 0;
                 $level_id = ($row['level_name']) ? $this->getBuildingLevelId($site_id, $building_id, $row['level_name']) : 0;
@@ -47,10 +48,9 @@ class SiteTenantsImport implements ToCollection, WithHeadingRow
     public function getBrandId($brand)
     {
         if($brand) {
-            dd($brand);
             $brand_id = Brand::where('name', '=', rtrim(ltrim($brand)))->first();
             if($brand_id)
-                return $brand_id['id'];
+                return $brand_id->id;
             return 0;
         }
 
