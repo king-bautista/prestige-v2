6ebcd4b0006a69274dadff91ba5b42803d1f06cb
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-07-31 10:54:30 +0800

    updates

--- app/Models/ViewModels/SitePointViewModel.php
+++ app/Models/ViewModels/SitePointViewModel.php
@@ -54,9 +54,12 @@ class SitePointViewModel extends Model
         if($this->point_label)
             return $this->point_label;
 
-        if($this->tenant_id) {
-            $site_tenant = SiteTenant::find($this->tenant_id);
-            return Brand::find($site_tenant->brand_id)->name;
+        $site_tenant = SiteTenant::find($this->tenant_id);
+        if($site_tenant) {
+            $brand = Brand::find($site_tenant->brand_id);
+            if($brand)
+                return $brand->name;
+            return null;
         }
         return null;
     }
