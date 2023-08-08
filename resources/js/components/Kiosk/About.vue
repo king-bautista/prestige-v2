459fb212039ff40200f3b8d79ce4ef91326ce4e6
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-08-08 18:11:11 +0800

    updates

--- resources/js/components/Kiosk/About.vue
+++ resources/js/components/Kiosk/About.vue
@@ -1,24 +1,48 @@
 <template>
     <div v-bind:class="(site_orientation == 'Portrait') ? 'router-page-portrait': 'router-page'" style="width: 100%;">
+        <div v-if="site_name == 'Parqal'" class="row">
+            <template v-if="site_orientation == 'Portrait'">
+                <div class="col-md-12 text-center">
+                    <div class="datetime-holder">
+                        <span class="separator">{{ current_time }}</span><span class="ml-3">{{ current_date }}</span>
+                    </div>
+                </div>
+            </template>
+            <template v-else>
+                <div class="col-md-12 text-right">
+                    <div class="datetime-holder mt-4 mb-3 ml-4">
+                        <span class="separator">{{ current_time }}</span><span class="ml-3">{{ current_date }}</span>
+                    </div>
+                </div>
+            </template>
+        </div>
         <div class="row m-0 p-0">
-            <div class="col-12 m-0 p-0">
-                <img :src="site_details.site_banner_path" style="max-width: 100%;">
+            <div class="col-md-10 offset-md-1 m-0 p-0">
+                <img :src="site_details.site_banner_path" style="width: 100%;">
             </div>
         </div>
         <div class="row">
-            <div class="col-6 col-sm-4 mt-50">
-                <div class="text-center" style="margin: 80px 0 80px 0;">
-                    <img :src="site_details.site_logo_path">
+            <template v-if="site_name == 'Parqal'">
+                <div class="col-md-10 offset-md-1">
+                    <div class="mall-details" v-html="site_details.descriptions">
+                    </div>
                 </div>
-                <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-facebook.svg" class="socmedicon mr-2"> {{ facebook }}</div>            
-                <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-twitter.svg" class="socmedicon mr-2"> {{ twitter }}</div>
-                <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-instagram.svg" class="socmedicon mr-2"> {{ instagram }}</div>
-                <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-website.svg" class="socmedicon mr-2"> {{ website }}</div>
-            </div>
-            <div class="col-6 col-sm-8">
-                <div class="mall-details" v-html="site_details.descriptions">
-			    </div>
-            </div>
+            </template>
+            <template v-else>
+                <div class="col-6 col-sm-4 mt-50">
+                    <div class="text-center" style="margin: 80px 0 80px 0;">
+                        <img :src="site_details.site_logo_path">
+                    </div>
+                    <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-facebook.svg" class="socmedicon mr-2"> {{ facebook }}</div>            
+                    <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-twitter.svg" class="socmedicon mr-2"> {{ twitter }}</div>
+                    <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-instagram.svg" class="socmedicon mr-2"> {{ instagram }}</div>
+                    <div class="socmediconcontainer" style="font-size: 1.3em"><img src="assets/images/about-website.svg" class="socmedicon mr-2"> {{ website }}</div>
+                </div>
+                <div class="col-6 col-sm-8">
+                    <div class="mall-details" v-html="site_details.descriptions">
+                    </div>
+                </div>
+            </template>
         </div>
         <div class="back-button" :src="back_button" @click="goBack"></div>
         <div class="back-overlay translateme" data-en="Back" @click="goBack">Back</div>
@@ -37,15 +61,25 @@
                 instagram: '',
                 website: '',
                 trigger_from: '',
+                site_name: '',
                 site_orientation: '',
             };
         },
 
         created() {
             this.getSite();
+            setInterval(this.getDateNow, 1000);
         },
 
         methods: {
+            getDateNow: function() {
+                const today = new Date();
+                const date = today.toLocaleString([], { day:"numeric", month:"long", year:"numeric"});
+                const time = today.toLocaleString([], {hour: '2-digit', minute:'2-digit'});
+                this.current_date = date;
+                this.current_time = time;
+            },
+
             getSite: function() {
 				axios.get('/api/v1/site')
                 .then(response => {
@@ -55,6 +89,8 @@
                         this.twitter = this.site_details.details.twitter;
                         this.instagram = this.site_details.details.instagram;
                         this.website = this.site_details.details.website;
+                        this.site_name = response.data.data.name
+                        this.site_orientation = response.data.data.site_orientation
                     }
                 });
 			},
