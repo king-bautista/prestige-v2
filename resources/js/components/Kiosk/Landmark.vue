a04ec6c909cef1879fb82800b9fc163b398befbd
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-08-11 17:36:02 +0800

    updates

--- resources/js/components/Kiosk/Landmark.vue
+++ resources/js/components/Kiosk/Landmark.vue
@@ -61,7 +61,7 @@
                     <img :src="landmark_details.image_url_path" class="lanmark-img">
                 </div>
                 <div v-bind:class="(site_orientation == 'Portrait') ? 'col-sm-6 mt-150': 'col-sm-7'" >
-                    <div class="lanmark-detail-holder m-5">
+                    <div class="lanmark-detail-holder m-5 mt-100">
                         <h2>{{ landmark_details.landmark }}</h2>
                         <p>{{ landmark_details.descriptions }}</p>
                         <p>{{ landmark_details.name }}</p>
