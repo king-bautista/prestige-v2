f9fc1594677166f116d4c2e7f6322871ed735d56
King Bautista <kbautista.prestigeinteractive@gmail.com>
2023-09-23 14:41:53 +0800

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -209,9 +209,9 @@ const app = new Vue({
     },
 
     mounted() {
-        document.addEventListener('touchstart', this.getTouchPoints());
-        document.addEventListener('touchend', this.getTouchPoints());
-        document.addEventListener('touchmove', this.getTouchPoints());
+        document.addEventListener('touchstart', this.getTouchPoints);
+        document.addEventListener('touchend', this.getTouchPoints);
+        document.addEventListener('touchmove', this.getTouchPoints);
         this.enableInterceptor();
     },
 
