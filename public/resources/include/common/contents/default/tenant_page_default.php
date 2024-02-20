a82cdfe744c348bf6aff8707d18f5644fd786577
King Bautista <kbautista.prestigeinteractive@gmail.com>
2024-02-20 11:33:44 +0800

    updates

--- public/resources/include/common/contents/default/tenant_page_default.php
+++
@@ -1,9 +0,0 @@
-<!-- Category Banner -->
-<div class="Category-Container-Banner">
-    <img class="category-img-banner" src="resources/uploads/categories/food/strips/kiosk/Food Hall.png">
-    <div class="hts-strip-align hts-strip-color">Food Hall</div>
-</div>
-
-    <div class="d-flex justify-content-center">
-        <img class="ImgPromoDefault2" src="resources\uploads\promos\stick-around.png">
-    </div>
--- public/resources/include/common/contents/directory_search.php
+++
@@ -1,7 +0,0 @@
-<!-- back btn -->
-<!--Start of the directory back button-->
-<?php include('resources/include/common/navigation/back_button.php'); ?>
-<!--Start of the directory back button-->
-
-<!-- title -->
-<div class="p-3 font-weight-bold nav-titles">Search</div>
